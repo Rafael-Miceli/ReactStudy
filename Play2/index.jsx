20ee78495b1ecfb5cd9c377d825380228a32483b
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2016-07-20 15:04:02 -0300

    Play 2 geting from github api

--- Play2/index.jsx
+++ Play2/index.jsx
@@ -1,11 +1,31 @@
 
+var Developer = React.createClass({
+  getInitialState(){
+    return {};
+  },
+  componentDidMount: function() {
+    var component = this;
+    $.get("https://api.github.com/users/" + this.props.username, function(data){
+      component.setState(data);
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <img src={this.state.avatar_url} alt={this.props.username} width="80"/>
+        <h3>{this.state.name}</h3>
+      </div>
+    )
+  }
+
+});
 
 var App = React.createClass({
   
   render: function() {
     return(
         <div>
-          Main Component
+          <Developer username="rafael-miceli"/>
         </div>
     )
   }
