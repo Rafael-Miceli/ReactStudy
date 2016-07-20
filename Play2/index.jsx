a6ac05ece82615d94472af40fdac2189a0c33fd3
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2016-07-20 11:07:05 -0300

    - Cleaning play 2 trash

--- Play2/index.jsx
+++ Play2/index.jsx
@@ -1,41 +1,15 @@
 
-var ButtonCounter = React.createClass({  
-  localClick: function () {
-    this.props.localClick(this.props.valueToIncrement);    
-  },
-  render: function() {
-    return (
-      <button onClick={this.localClick}>Clicked me {this.props.valueToIncrement}</button>
-    )
-  }
-});
-
-var CounterDisplay = React.createClass({
-  render: function() {
-    return (
-      <h2>{this.props.valueToDisplay}</h2>
-    )
-  }
-});
 
 var App = React.createClass({
-  getInitialState: function() {
-    return {totalClicks: 0};
-  },
-  handleClick: function(increment) {
-    this.setState({totalClicks: this.state.totalClicks + increment})
-  },
+  
   render: function() {
     return(
         <div>
-          <ButtonCounter localClick={this.handleClick} valueToIncrement={2}/>
-          <ButtonCounter localClick={this.handleClick} valueToIncrement={10}/>
-          <ButtonCounter localClick={this.handleClick} valueToIncrement={20}/>
-          <ButtonCounter localClick={this.handleClick} valueToIncrement={50}/>
-          <CounterDisplay valueToDisplay={this.state.totalClicks}/>
+          Main Component
         </div>
     )
   }
+
 });
 
 ReactDOM.render(<App />, document.getElementById("mainPlay"));
