80ec8db035f7fc4e71d01d7fc527b2e20f5d3825
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2016-07-26 11:05:25 -0300

    Solution applied because of react native issue

--- ReactNative/AwesomeProject/login.js
+++ ReactNative/AwesomeProject/login.js
@@ -20,6 +20,15 @@ export class Login extends Component {
     }
 
     render() {
+        if (this.state.showProgress) {
+            return this.renderLoadingView();
+        }
+        else {
+            return this.renderViewWithouLogin();
+        }
+    }
+
+    renderLoadingView() {
         return (
             <View>
                 <Text>
@@ -34,16 +43,55 @@ export class Login extends Component {
                     <Text>Login</Text>
                 </TouchableHighlight>
 
-                <ActivityIndicator animating={this.state.showProgress} size="large"/>
+                <TouchableHighlight onPress={this.onAct.bind(this)}>
+                    <Text>Activity</Text>
+                </TouchableHighlight>
+
+                <ActivityIndicator animating={true} size="large" />              
+            </View>                
+        )
+    }
+
+    renderViewWithouLogin() {
+        return (
+            <View>
+                <Text>
+                    Realize seu login
+                </Text>
+                <TextInput placeholder="Login" 
+                onChangeText={(text) => this.setState({username: text})} />
+                <TextInput placeholder="Password" secureTextEntry={true} 
+                onChangeText={(text) => this.setState({password: text})} />
+
+                <TouchableHighlight onPress={this.onLogin.bind(this)}>
+                    <Text>Login</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight onPress={this.onAct.bind(this)}>
+                    <Text>Activity</Text>
+                </TouchableHighlight>              
             </View>
-        );
+        )
+        
     }
 
     onLogin() {
         console.log("Ususario: ", this.state.username);
         console.log("Senha: ", this.state.password);
+        console.log("Antes de tentar logar: ", this.state.showProgress);
+        this.setState({showProgress: true}); 
+        console.log("Após tentar logar: ", this.state.showProgress);
+    }
 
-        this.state.showProgress = true; 
+    onAct() {
+        console.log("Antes de tentar active: ", this.state.showProgress);
+        this.setState({showProgress: false}); 
+        console.log("Após tentar active: ", this.state.showProgress);
     }
 }
 
+const styles = StyleSheet.create({
+    loader: {
+        marginTop: 200
+    }
+});
